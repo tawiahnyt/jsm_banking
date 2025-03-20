a8e5701ec034d6cfc55962696eb64bfb8f4c50da
tawiah <tawiahin4k@gmail.com>
2025-03-20 15:23:08 +0000

    :fire: add mobile navigation and sheet components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,8 @@
 /** @format */
 
+import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 
 export default function RootLayout({
@@ -10,9 +12,23 @@ export default function RootLayout({
 }>) {
   const loggedIn = { firstName: "tawiah", lastname: 'in4k' };
   return (
-    <main className='flex h-screen w-full font-inter'>
-        <Sidebar user={loggedIn} />
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image
+            src={"/icons/logo.svg"}
+            width={30}
+            height={30}
+            alt="menu icon"
+          />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
         {children}
+      </div>
     </main>
   );
 }
