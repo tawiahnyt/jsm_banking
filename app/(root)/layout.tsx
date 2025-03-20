900180eacc5bb7e7fb1ac3af60787c603ea84f54
tawiah <tawiahin4k@gmail.com>
2025-03-20 15:12:53 +0000

    :zap: add Sidebar component to root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,17 @@
 /** @format */
 
+import Sidebar from "@/components/Sidebar";
+
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "tawiah", lastname: 'in4k' };
   return (
-    <main>
-        SIDEBAR
+    <main className='flex h-screen w-full font-inter'>
+        <Sidebar user={loggedIn} />
         {children}
     </main>
   );
